65665c8cc74ff3582fdb860a4aa160ebdbd74e5e
JJL Computer Solutions <support@jjlsolutions.com>
2026-03-19 10:36:45 -0400

    Disable update check and version upgrade prompt for custom build

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -468,7 +468,8 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           await rustDeskWinManager.closeAllSubWindows();
           bind.mainGotoInstall();
         });
-      } else if (bind.mainIsInstalledLowerVersion()) {
+      } else if (false && bind.mainIsInstalledLowerVersion()) {
+        // Disabled for JJL Remote Support custom build
         return buildInstallCard(
             "Status", "Your installation is lower version.", "Click to upgrade",
             () async {
